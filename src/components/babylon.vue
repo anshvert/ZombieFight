a7c0d97e4fd1ae966066c44e51ecc0b000dd3387
Ansh Tyagi <anshtyagi2222@gmail.com>
2023-05-14 18:40:59 +0530

    Babylon Init

---
+++ src/components/babylon.vue
@@ -0,0 +1,33 @@
+<template>
+  <h3>Babylon Expert</h3>
+  <canvas></canvas>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Babylon',
+    mounted() {
+      const canvas = document.querySelector("canvas")
+    }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
